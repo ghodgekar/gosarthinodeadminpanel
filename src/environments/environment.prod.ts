2a0a6532380b5478a4a3aac0dfc5c2e6ff84ad03
surabhi ghodgekar <surabhighodgekar30@gmail.com>
2022-07-17 11:22:10 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-    production: true
+    production: true,
+    firebaseConfig:{
+      apiKey: "AIzaSyBMU2bYjjvh_YN5dKZBQe5XoaPa0Ij3r24",
+      authDomain: "gosarthiinew.firebaseapp.com",
+      projectId: "gosarthiinew",
+      storageBucket: "gosarthiinew.appspot.com",
+      messagingSenderId: "407863203622",
+      appId: "1:407863203622:web:aa126e91b49eba060313e7",
+      measurementId: "G-73BT08DY2N"
+    }
 };
